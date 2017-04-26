51d02ca1551e2cccc9cc0bbc763b7852f69ebc95
Jacques Begin <jacques.andre.begin@gmail.com>
2017-04-26 22:04:10 +0000

    Creating database tables

--- db/migrations/20160712210838_create_users_table.js
+++ db/migrations/20160712210838_create_users_table.js
@@ -1,9 +1,96 @@
 exports.up = function(knex, Promise) {
-  return knex.schema.createTable('users', function (table) {
-    table.increments();
-    table.string('name');
-  });
+return CreateUserTable();
+  .then(CreateDeckTable)
+  .then(CreateGenreTable)
+  .then(CreateSlidesTable)
+  .then(CreateAnswerTable)
+  .then(CreateDeckSlidesTable)
+  .then(CreateUserDeckScoreTable)
+
+
+  function CreateUserTable() {
+    return knex.schema.createTable('users', function (table) {
+      table.increments('id');
+      table.string('firstname').notNullable();
+      table.string('lastname').notNullable();
+      table.string('email').notNullable().unique();
+      table.string('password').notNullable();
+      table.dateTime('createDate').notNullable();
+      table.dateTime('updateDate');
+      table.string('cookie');
+    });
+  }
+
+  // add foreign keys user and genre
+  function CreateDeckTable() {
+    return knex.schema.createTable('deck', function (table) {
+      table.increments('id');
+      table.string('name').notNullable();
+      table.integer('userid(creator)').notNullable();
+      table.dateTime('createDate').notNullable();
+      table.dateTime('updateDate');
+      table.integer('genreid');
+    });
+  }
+
+  function CreateGenreTable() {
+    return knex.schema.createTable('genre', function (table) {
+      table.increments('id');
+      table.string('name').notNullable();
+      table.text('description').notNullable();
+    });
+  }
+
+  // add foreign keys genre
+  function CreateSlidesTable() {
+    return knex.schema.createTable('slides', function (table) {
+      table.increments('id');
+      table.string('question').notNullable();
+      table.integer('genreid').notNullable();
+      table.dateTime('createDate');
+      table.dateTime('updateDate');
+      table.string('pictureUrl');
+      table.string('correctAnswer').notNullable();
+    });
+  }
+
+  function CreateAnswerTable() {
+    return knex.schema.createTable('answer', function (table) {
+      table.increments('slideId');
+      table.string('answer').notNullable();
+      table.boolean('correctAnswer').notNullable();
+      table.integer('effect');
+    });
+  }
+
+
+  // needs to implement foreign keys for deck and slide
+  function CreateDeckSlidesTable() {
+    return knex.schema.createTable('deckSlides', function (table) {
+      table.integer('deckid');
+      table.integer('slideid');
+    });
+  }
+
+  // needs to implement foreign keys for deck and user
+  function CreateUserDeckScoreTable() {
+    return knex.schema.createTable('userDeckScore', function (table) {
+      table.integer('deckid');
+      table.integer('userid');
+      table.integer('correctAnswerCount');
+    });
+  }
+
+
 };
+
+
+// function (knex, Promise) {
+//   return knex.schema.createTable('users', function (table) {
+//     table.increments();
+//     table.string('name');
+//   });
+// };
 
 exports.down = function(knex, Promise) {
   return knex.schema.dropTable('users');
