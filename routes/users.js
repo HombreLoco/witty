5015ff0b3608c4855e0a2d4378934b324e0619ca
Keawe <account@keawewong.com>
2017-04-29 17:02:02 -0400

    Fixed page positioning. Moving on to home page layout.

--- routes/users.js
+++ routes/users.js
@@ -2,7 +2,7 @@
 
 const express         = require('express');
 const router          = express.Router();
-const bcrypt          = require('bcrypt');
+// const bcrypt          = require('bcrypt');
 
 module.exports = (knex) => {
 
--- server.js
+++ server.js
@@ -34,7 +34,6 @@ app.use(cookieSession ({
   keys: ["keys1", "keys2"]
 }))
 
-
 // Load the logger first so all (static) HTTP requests are logged to STDOUT
 // 'dev' = Concise output colored by response status for development use.
 //         The :status token will be colored red for server error codes, yellow for client error codes, cyan for redirection codes, and uncolored for all other codes.
