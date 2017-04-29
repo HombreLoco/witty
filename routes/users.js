782dc7ad132b03b87c7f0a5c6830c7628b5e3cec
Jacques Begin <jacques.andre.begin@gmail.com>
2017-04-29 17:52:51 +0000

    Adds validation for whether email and password are empty in the server route and changes jquery reference name to login form information.

--- routes/users.js
+++ routes/users.js
@@ -29,27 +29,27 @@ module.exports = (knex) => {
     });
   });
 
-  // router.post("/login", (req, res) => {
-  //     if (!req.body.email || !req.body.password) {
-  //       res.status(400).json({ error: 'invalid request: no data in form'});
-  //       return;
-  //     }
-  //     knex('users').where({
-  //       email: req.body.email,
-  //     }).select('firstname', 'password')
-  //     .then((results) => {
-  //       //print hello (name). on test page results[0].firstname
-  //       if(results.length === 0){
-  //         res.redirect("/");
-  //       } else {
-  //         if(bcrypt.compareSync(req.body.password, results[0].password)) {
-  //           res.redirect("/");
-  //         } else {
-  //           res.status(400).json({ error: 'invalid details'}); //print error message on registration page
-  //         }
-  //       }
-  //     })
-  //   });
+  router.post("/login", (req, res) => {
+      if (!req.body.email || !req.body.password) {
+        res.status(400).json({ error: 'invalid request: no data in form'});
+        return;
+      }
+      knex('users').where({
+        email: req.body.email,
+      }).select('firstname', 'password')
+      .then((results) => {
+        //print hello (name). on test page results[0].firstname
+        if(results.length === 0){
+          res.redirect("/");
+        } else {
+          if(bcrypt.compareSync(req.body.password, results[0].password)) {
+            res.redirect("/");
+          } else {
+            res.status(400).json({ error: 'invalid details'}); //print error message on registration page
+          }
+        }
+      })
+    });
 
   router.post("/logout", (req, res) => {
     req.session = null;
