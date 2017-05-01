543b1755c01d3395fe6b26323b973041a884a6b4
Keawe <account@keawewong.com>
2017-05-01 12:38:43 -0400

    Project wrapped. Pushed by Keawe.

--- public/scripts/home.js
+++ public/scripts/home.js
@@ -1,5 +1,7 @@
 $(document).ready(function() {
 
+
+
   const htmlWinner = [`
       <div class="answerEffect" id="winner1">
                 <img src="winnerEffect/1.gif" class="img-responsive center-block" alt="winner1">
